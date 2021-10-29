176eee528be42e5ceb94830eff23ae0e459d4f92
Tom <timakadirov406@gmail.com>
2021-10-29 14:59:25 +0500

    add done and important funcs

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,25 +4,42 @@ import './todo-list-item.css';
 
 export default class TodoListItem  extends Component {
 
-    render() {
-        const { label, important = false} = this.props;
+    onLabelClick = () => {
+        this.setState({
+            done: true,
+            important: false
+        })
+    }
 
-        const style = {
-            color: important ? "steelblue" : "black", 
-            fontWeight: important ? 'bold' : 'normal'
-        };
+    onExclamationMark = () => {
+        this.setState({
+            important: true
+        })
+    }
+
+    state = { 
+        done: false
+    };
+
+    render() {
+        const { label } = this.props;
+        const { done, important } = this.state;
+
+        let classNames = "todo-list-item";
+        if (done) classNames += " done";
+        if (important) classNames += " important";
 
         return ( 
-            <span className="todo-list-item">
+            <span className={classNames}>
                 <span 
                     className="todo-list-item-label"
-                    style={style}
-                    onClick={ () => console.log(`Done: ${label}`) }>
+                    onClick={ this.onLabelClick }>
                     { label }
                 </span>
         
                 <button type="button"
-                    className="btn btn-outline-success btn-sm float-right">
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={ this.onExclamationMark }>
                     <i className="fa fa-exclamation" />
                 </button>
         
