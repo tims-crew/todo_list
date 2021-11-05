27d7608c5d89a44c33a21403e92181043ea1d5ff
Tom <timakadirov406@gmail.com>
2021-11-05 23:52:51 +0500

    Used setState(), Added new features

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,18 +4,66 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import AddItemForm from '../add-item-form';
 
 import './app.css'
 
+// ADD: realize undo button, it saves the deleted during 12 days and after that removes from list of deleted tasks
+
 export default class App extends Component {
-  
-  state =  {
-    todoData: [
-      { id: 1, label: "Drink Coffee", important: false },
-      { id: 2, label: "Make Awesome App", important: true },
-      { id: 3, label: "Learn React", important: false }
-    ]
-  };
+  maxId = 100;
+    
+  createNewItem = (label) => {
+    return {
+      label,
+      done: false,
+      important: false,
+      id: this.maxId++
+    }
+  }
+
+  addItem = text => {
+    this.setState(({todoData}) => {
+      return {
+        todoData: [
+          ...todoData,
+          this.createNewItem(text)
+        ]
+      }
+    })
+  }
+
+  onToggleImportant = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+      const oldArr = todoData[idx];
+      const newArr = { ...oldArr, important: !oldArr.important};
+
+      return {
+        todoData: [ 
+          ...todoData.slice(0, idx),
+          newArr,
+          ...todoData.slice(idx + 1)      
+        ]
+      }
+    })
+  }
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+      const oldArr = todoData[idx];
+      const newArr = { ...oldArr, done: !oldArr.done};
+
+      return {
+        todoData: [ 
+          ...todoData.slice(0, idx),
+          newArr,
+          ...todoData.slice(idx + 1)      
+        ]
+      }
+    })
+  }
 
   deleteItem = id => {
     this.setState(({ todoData }) => {
@@ -31,19 +79,35 @@ export default class App extends Component {
       }
     })
   }
-    
+
+  state = {
+    todoData: [
+      this.createNewItem('Drink Coffee'),
+      this.createNewItem('Make Awesome App'),
+      this.createNewItem('Learn React')
+    ]
+  };
+
   render() {
+    const { todoData } = this.state;
+
+    const doneCount = todoData.filter(el => el.done).length;
+    const todoCount = todoData.length - doneCount;
+    
     return (
       <div className="todo-app">
-        <AppHeader todo={3} done={0}/>
+        <AppHeader todo={todoCount} done={doneCount}/>
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
         
         <TodoList 
-          todos = { this.state.todoData }
-          onDeleted={ this.deleteItem } />
+          todos = { todoData }
+          onDeleted={ this.deleteItem } 
+          onToggleDone={ this.onToggleDone }
+          onToggleImportant={ this.onToggleImportant }/>
+        <AddItemForm onItemAdded={ this.addItem }/>
       </div>
     );
   };
